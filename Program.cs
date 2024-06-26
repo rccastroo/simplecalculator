84c0a0dc2f6c1f85a95dd0ce61752ecc111d444d
rccastroo <rj_ruan@hotmail.com>
2024-06-26 16:17:22 -0300

    finished project

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite O segundo número: ");
+            int numeber2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Selecione a operação: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            int operation = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            switch (operation)
+            {
+                case 1:
+                    result = number1 + numeber2;
+                    break;
+                case 2:
+                    result = number1 - numeber2;
+                    break;
+                case 3:
+                    result = number1 * numeber2;
+                    break;
+                case 4:
+                    result = number1 / numeber2;
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida.");
+                    break;
+            }
+
+            Console.WriteLine("O resultado é: " + result);
+        }
+        
+    }
+}
